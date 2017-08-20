a07756c2a3b7369ed53ddd01b6f5d3f3e775311f
Connor Taylor <connor.taylor@icloud.com>
2017-08-20 13:20:28 +1200

    Upload Unity Project

---
+++ Assets/Sketch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+	public GameObject myPrefab;
+
+	void Start () {
+
+		int totalCubes = 30;
+
+		float totalDistance = 2.9f;
+
+		for ( int i = 0; i < totalCubes; i++)
+		{
+			float perc = i / (float)totalCubes;
+			float sin = Mathf.Sin(perc * Mathf.PI/2);
+
+			float x = 1.8f + sin * totalDistance;
+			float y = 5.0f;
+			float z = 0.0f;
+
+			var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+
+			newCube.GetComponent<myCubeScript>().SetSize(.45f * (1.0f - perc));
+			newCube.GetComponent<myCubeScript>().rotateSpeed = .2f + perc * 4.0f;
+		}
+
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/myCubeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myCubeScript : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinSpeed = Vector3.zero;
+	public Vector3 spinAxis = new Vector3 (0, 1, 0);
+
+	void Start () {
+		spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+		spinAxis = Vector3.up;
+		spinAxis.x = (Random.value - Random.value) * .1f;
+	}
+
+	public void SetSize(float size)
+	{
+		this.transform.localScale = new Vector3(size, size, size);
+	}
+
+	void Update () {
+		this.transform.Rotate(spinSpeed);
+		this.transform.RotateAround (Vector3.zero, spinAxis, rotateSpeed);
+	}
+}
